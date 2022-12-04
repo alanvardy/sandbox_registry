ca65ddf0ecdfed39a23fcc502023c81377245590
MikaAK <mika@kalathil.me>
2022-12-04 15:00:19 -0800

    chore: bump to 0.1.1.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.1
+- Remove map requirement for state
+
+# 0.1.0
+- Initial Release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SandboxRegistry.MixProject do
   def project do
     [
       app: :sandbox_registry,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       description: "Registry to help create sandboxes for testing",
       start_permanent: Mix.env() == :prod,
